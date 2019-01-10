9ca1eb14e2ce32755e792f21706e9139d3971f50
Denys Smirnov <denys@sourced.tech>
2019-01-10 22:47:55 +0200

    preprocess positional info
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- driver/normalizer/normalizer.go
+++ driver/normalizer/normalizer.go
@@ -1,18 +1,11 @@
 package normalizer
 
 import (
+	"gopkg.in/bblfsh/sdk.v2/uast"
 	. "gopkg.in/bblfsh/sdk.v2/uast/transformer"
 )
 
 var Preprocess = Transformers([][]Transformer{
-	{
-		// ResponseMetadata is a transform that trims response metadata from AST.
-		//
-		// https://godoc.org/gopkg.in/bblfsh/sdk.v2/uast#ResponseMetadata
-		ResponseMetadata{
-			TopLevelIsRootNode: false,
-		},
-	},
 	{Mappings(Preprocessors...)},
 }...)
 
@@ -23,12 +16,28 @@ var Normalize = Transformers([][]Transformer{
 
 // Preprocessors is a block of AST preprocessing rules rules.
 var Preprocessors = []Mapping{
-	// ObjectToNode defines how to normalize common fields of native AST
-	// (like node type, token, positional information).
-	//
-	// https://godoc.org/gopkg.in/bblfsh/sdk.v2/uast#ObjectToNode
+	Map(
+		Part("_", Obj{
+			"FullSpan": Obj{
+				uast.KeyType: String("TextSpan"),
+				"Length":     AnyNode(nil),
+				"Start":      Var("start"),
+				"End":        Var("end"),
+			},
+			// TODO(dennwc): add those as a custom position fields?
+			"Span":      AnyNode(nil),
+			"SpanStart": AnyNode(nil),
+		}),
+		Part("_", Obj{
+			// remap to temporary keys and let ObjectToNode to pick them up
+			"spanStart": Var("start"),
+			"spanEnd":   Var("end"),
+		}),
+	),
+
 	ObjectToNode{
-		InternalTypeKey: "...", // native AST type key name
+		OffsetKey:    "spanStart",
+		EndOffsetKey: "spanEnd",
 	}.Mapping(),
 }
 
