5f7ff94e702ee5ccb6b37690a3227ac6ffc7b2b8
Denys Smirnov <denys@sourced.tech>
2019-01-10 19:48:16 +0200

    basic native protocol support
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- native/Program.cs
+++ native/Program.cs
@@ -1,7 +1,22 @@
 using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
 
 namespace native
 {
+    public class ParseRequest
+    {
+        public string content;
+    }
+
+    public class ParseResponse
+    {
+        public string status;
+        public List<string> errors;
+        public Object ast;
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -9,8 +24,23 @@ namespace native
             string line;
             while ((line = Console.ReadLine()) != null)
             {
-                Console.Write("{\"status\":\"ok\", \"ast\": "+line+"}\n");
+                ParseRequest req = JsonConvert.DeserializeObject<ParseRequest>(line);
+
+                Object ast = Parse(req.content);
+
+                ParseResponse resp = new ParseResponse
+                {
+                    status = "ok",
+                    ast = ast,
+                };
+                string json = JsonConvert.SerializeObject(resp);
+                Console.Write(json);
             }
+        }
+
+        static Object Parse(string source)
+        {
+            return source; // TODO
         }
     }
 }
