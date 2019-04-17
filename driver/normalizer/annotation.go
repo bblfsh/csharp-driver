fd044547a6c63bbed9d3cd68d71adbb5a92fc70f
Denys Smirnov <denys@sourced.tech>
2019-04-17 18:43:53 +0300

    switch to go modules
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- driver/normalizer/annotation.go
+++ driver/normalizer/annotation.go
@@ -1,16 +1,16 @@
 package normalizer
 
 import (
-	"gopkg.in/bblfsh/sdk.v2/uast"
-	"gopkg.in/bblfsh/sdk.v2/uast/role"
+	"github.com/bblfsh/sdk/v3/uast"
+	"github.com/bblfsh/sdk/v3/uast/role"
 
-	. "gopkg.in/bblfsh/sdk.v2/uast/transformer"
-	"gopkg.in/bblfsh/sdk.v2/uast/transformer/positioner"
+	. "github.com/bblfsh/sdk/v3/uast/transformer"
+	"github.com/bblfsh/sdk/v3/uast/transformer/positioner"
 )
 
 // Native is the of list `transformer.Transformer` to apply to a native AST.
 // To learn more about the Transformers and the available ones take a look to:
-// https://godoc.org/gopkg.in/bblfsh/sdk.v2/uast/transformer
+// https://godoc.org/github.com/bblfsh/sdk/v3/uast/transformer
 var Native = Transformers([][]Transformer{
 	// The main block of transformation rules.
 	{Mappings(Annotations...)},
@@ -25,7 +25,7 @@ var Native = Transformers([][]Transformer{
 // and can access original source code file. It can be used to improve or
 // fix positional information.
 //
-// https://godoc.org/gopkg.in/bblfsh/sdk.v2/uast/transformer/positioner
+// https://godoc.org/github.com/bblfsh/sdk/v3/uast/transformer/positioner
 var PreprocessCode = []CodeTransformer{
 	positioner.FromOffset(),
 	positioner.TokenFromSource{
@@ -36,8 +36,6 @@ var PreprocessCode = []CodeTransformer{
 		},
 	},
 }
-
-var Code []CodeTransformer // legacy stage, will be deprecated
 
 // Annotations is a list of individual transformations to annotate a native AST with roles.
 var Annotations = []Mapping{
